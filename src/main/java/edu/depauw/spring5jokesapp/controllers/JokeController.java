9d75309af026e121068604feac890fda910f5e9a
gordianbruns <gordob1@web.de>
2021-06-09 08:33:50 -0400

    Added controller

--- src/main/java/edu/depauw/spring5jokesapp/controllers/JokeController.java
+++ src/main/java/edu/depauw/spring5jokesapp/controllers/JokeController.java
@@ -1,2 +1,24 @@
-package edu.depauw.spring5jokesapp.controllers;public class JokeController {
+package edu.depauw.spring5jokesapp.controllers;
+
+import edu.depauw.spring5jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
 }
